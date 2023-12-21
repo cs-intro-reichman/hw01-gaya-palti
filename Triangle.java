a7c43c446dd72e4cec600352f892ec60a162c9c1
gaya-palti <gaya.palti@post.runi.ac.il>
2023-12-21 15:48:14 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -12,6 +12,6 @@ public class Triangle {
     int sumAC = a + c;
     int sumBC = b + c;
     boolean isTriangle = (sumAB >= c && sumAC >= b && sumBC >= a);
-    System.out.println(a + " , " + b + " , " + c + " : " + isTriangle );
+    System.out.println(a + " ," + b + " ," + c + " :" + isTriangle );
 	}
 }
